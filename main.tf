aa5b61bb46c91db51db8a52a5e00d1c2e8fd4165
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-16 19:58:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
-resource "aws_s3_account_public_access_block" "this" {
-  count = var.create ? 1 : 0
+resource "aws_s3_bucket" "example" {
+  bucket = "my-subodh-bucket"
 
-  account_id = var.account_id
-
-  block_public_acls       = var.block_public_acls
-  block_public_policy     = var.block_public_policy
-  ignore_public_acls      = var.ignore_public_acls
-  restrict_public_buckets = var.restrict_public_buckets
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
+
+
+
+
+
+
+
+
+
+
