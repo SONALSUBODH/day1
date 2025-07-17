6f504fcca66b805b8601e141cc49f5f423d5357b
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-17 21:23:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,6 @@
-# The default provider configuration; resources that begin with `aws_` will use
-# it as the default, and it can be referenced as `aws`.
 provider "aws" {
   region = "ap-south-1"
+  access_key = ""
+  secret_key = ""
 }
 
-# Additional provider configuration for west coast region; resources can
-# reference this as `aws.south`.
-provider "aws" {
-  alias  = "south"
-  region = "ap-south-1"
-}
